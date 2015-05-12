b24807d1270b56f84bfe43b68e0294ba1243b42a
Irene Sanz <irene.sanz@bq.com>
2015-05-12 17:36:01 +0200

    Sending a command that uses the serial port kills the serial monitor if opened. Serial monitor now has pause/resume and clear button. Improved data reception through serial port.

--- src/SerialMonitor.py
+++ src/SerialMonitor.py
@@ -30,7 +30,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 import unicodedata
 
-qBoard = Queue()
+qBoard = Queue(maxsize=1)
 qMonitor = Queue()
 qBaudRate = Queue()
 
@@ -46,6 +46,8 @@ class SerialMonitor:
 			stopbits=serial.STOPBITS_TWO,
 			bytesize=serial.SEVENBITS
 		)
+		self.pause = False
+
 		self.t2 = threading.Thread(target=self.ui)
 		self.t2.start()
 
@@ -58,6 +60,7 @@ class SerialMonitor:
 		self.t4 = threading.Thread(target=self.readBaudRate)
 		self.t4.start()
 
+
 	def readQmonitor(self):
 		while 1:
 			if not qMonitor.empty():
@@ -67,6 +70,10 @@ class SerialMonitor:
 					time.sleep(0.5)
 					os._exit(1)
 					return
+				elif message == 'stopWeb2boardPythonProgram':
+					self.pause = True
+				elif message == 'startWeb2boardPythonProgram':
+					self.pause = False
 				else:
 					self.serial.write(message)
 				qMonitor.task_done()
@@ -74,15 +81,15 @@ class SerialMonitor:
 
 	def readBoard (self):
 		while 1 :
-			out = ''
-			try:
-				while self.serial.inWaiting() > 0:
-					out += self.serial.read(1)
-					time.sleep(0.01)
-				if out != '':
-					qBoard.put(out)
-			except:
-				pass
+			if self.serial.isOpen():
+				out = ''
+				try:
+					while self.serial.inWaiting() > 0:
+						out += self.serial.read(1)
+					if out != '' and self.pause == False:
+						qBoard.put(out)
+				except:
+					pass
 
 	def ui (self):
 		ex = wx.App()
@@ -92,7 +99,12 @@ class SerialMonitor:
 	def readBaudRate (self):
 		while 1:
 			if not qBaudRate.empty():
-				self.serial.setBaudrate(qBaudRate.get())
+				baudRate = qBaudRate.get()
+				self.serial.close()
+				time.sleep(1)
+				self.serial.baudrate = baudRate
+				time.sleep(1)
+				self.serial.open()
 
 
 class SerialMonitorUI(wx.Dialog):
@@ -103,25 +115,27 @@ class SerialMonitorUI(wx.Dialog):
 		#Timer
 		self.timer = wx.Timer(self)
 		self.Bind(wx.EVT_TIMER, self.update, self.timer)
-		self.timer.Start(10)
-		self.newLine = '\n'
-
+		self.timer.Start(100)
+		self.newLine = ''
+		self.Pause = False
+		self.charCounter = 0
 
 		# Elements
 		self.inputTextBox = wx.TextCtrl(self, size=(300,10))
 		self.response = wx.TextCtrl(self, size=(10,250), style=wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_AUTO_URL)
+		self.response.SetMaxLength(3)
 		self.sendButton = wx.Button(self, label='Send',style = wx.ALIGN_RIGHT)
-		
-		self.checkBox = wx.CheckBox(self, -1, 'NewLine', (10, 10))
-		self.checkBox.SetValue(True)
+		self.pauseButton = wx.Button(self, label='Pause',style = wx.ALIGN_RIGHT)
+		self.clearButton = wx.Button(self, label='Clear',style = wx.ALIGN_RIGHT)
 
 		baudRates = ['300', '1200', '2400', '4800', '9600', '14400','19200', '28800', '38400', '57600', '115200']
 		self.dropdown = wx.ComboBox(self, 0, '9600', (0, 0), wx.DefaultSize ,baudRates, wx.CB_DROPDOWN)
 
 		# Events
 		self.sendButton.Bind(wx.EVT_BUTTON, self.onSend)
+		self.pauseButton.Bind(wx.EVT_BUTTON, self.onPause)
+		self.clearButton.Bind(wx.EVT_BUTTON, self.onClear)
 		self.Bind(wx.EVT_CLOSE, self.onClose)
-		self.checkBox.Bind(wx.EVT_CHECKBOX, self.onBoxChanged)
 		self.dropdown.Bind(wx.EVT_COMBOBOX, self.onBaudRateChanged)
 
 		# Layout
@@ -130,33 +144,54 @@ class SerialMonitorUI(wx.Dialog):
 		hbox.Add(self.inputTextBox, 1,wx.ALL^wx.CENTER|wx.EXPAND, 12)
 		hbox.Add(self.sendButton, 0, wx.ALL^wx.ALIGN_RIGHT, 12)
 		vbox.Add(hbox, 0, wx.EXPAND, 12)
-		# vbox.Add(self.inputTextBox, 1, wx.ALL^wx.CENTER|wx.EXPAND, 12)
 		vbox.Add(self.response, 1, wx.ALL^wx.CENTER|wx.EXPAND, 12)
 		hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-		hbox1.Add(self.checkBox, 0, wx.ALL^wx.ALIGN_LEFT, 12)
+		hbox1.Add(self.pauseButton, 0, wx.ALL^wx.ALIGN_LEFT, 12)
+		hbox1.Add(self.clearButton, 0, wx.ALL^wx.CENTER|wx.EXPAND, 12)
 		hbox1.Add((0, 0), 1, wx.EXPAND)
-
 		hbox1.Add(self.dropdown, 0, wx.ALL^wx.ALIGN_RIGHT, 12)
 		vbox.Add(hbox1, 0, wx.EXPAND, 12)
 		self.SetSizer(vbox)
 		self.ShowModal()
 
 	def update(self, event):
-		if not qBoard.empty():
+		if not qBoard.empty() and self.Pause == False:
 			message = qBoard.get()
-			self.response.AppendText('\t'+message+self.newLine)
+			self.logText(message)
+
+	def logText (self, message):
+		if '\n' in message:
+			self.charCounter = 0
+		else:
+			self.charCounter+=1
+
+		if self.response.GetNumberOfLines() >= 800 or self.charCounter > 300:
+			self.response.SetValue(message)
+			self.charCounter = 0
+		else:
+			self.response.AppendText(message)
 
 	def onSend(self, event):
 		message = self.inputTextBox.GetValue()
-		self.response.AppendText(message+self.newLine)
+		self.logText(message)
+		# self.response.AppendText(message+self.newLine)
 		qMonitor.put(unicode(message))
 		self.inputTextBox.SetValue('')
 
-	def onBoxChanged (self, event):
-		if self.newLine =='\n':
-			self.newLine=''
+	def onPause (self, event):
+		if self.pauseButton.GetLabel() == 'Pause':
+			self.pauseButton.SetLabel('Continue')
+			qMonitor.put('stopWeb2boardPythonProgram')
+			self.Pause = True
 		else:
-			self.newLine='\n'
+			self.pauseButton.SetLabel('Pause')
+			qMonitor.put('startWeb2boardPythonProgram')
+			self.Pause = False
+
+	def onClear (self, event):
+		self.response.SetValue('')
+		self.inputTextBox.SetValue('')
+		self.charCounter = 0
 
 	def onBaudRateChanged (self, event):
 		qBaudRate.put(self.dropdown.GetValue())
