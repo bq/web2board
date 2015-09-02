b9d16f1fc84c3adc30f5f3f87d479153fda9ae43
Tom Calvo <calvo.tom@gmail.com>
2015-09-02 12:50:57 +0200

    remove pkg and added certificate and fix bitbloqlibs path on install

--- pkg/darwin/create-mpkg/web2board/bitbloqLibs.sh
+++ pkg/darwin/create-mpkg/web2board/bitbloqLibs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Create route if it does not exist previously
-mkdir -p $HOME/Arduino/libraries/
+mkdir -p $HOME/Documents/Arduino/libraries/
 
 #Remove previous bitbloqLibs library if it exists:
-rm -rf $HOME/Arduino/libraries/bitbloqLibs
+rm -rf $HOME/Documents/Arduino/libraries/bitbloqLibs
 
-cd $HOME/Arduino/libraries/
+cd $HOME/Documents/Arduino/libraries/
 
 #Download the new version:
 curl -L "https://github.com/bq/bitbloqLibs/archive/v0.0.1.zip"  -o "v0.0.1.zip"
