9cdab3b255dea327f1068a79836762c790b2dee1
Jorge García Irazábal <Jorge Garca Irazbal>
2015-12-29 10:12:35 +0100

    created MacPackger

--- src/libs/Packagers/MacPackager.py
+++ src/libs/Packagers/MacPackager.py
@@ -6,15 +6,15 @@ from libs.utils import *
 log = logging.getLogger(__name__)
 
 
-class WindowsPackager(Packager):
+class MacPackager(Packager):
     def __init__(self):
         Packager.__init__(self)
-        self.versionPath = self.web2boardPath + os.sep + "win_web2board_{}".format(self.version)
-        self.installerPath = self.installerFolder + os.sep + "win32"
-        self.winDist = os.path.join(self.versionPath, "dist")
+        self.versionPath = self.web2boardPath + os.sep + "darwin_web2board_{}".format(self.version)
+        self.installerPath = self.installerFolder + os.sep + "darwin"
+        self.macDist = os.path.join(self.versionPath, "dist")
         self.folderVersionName = os.path.basename(self.versionPath)
-        self.winMetadataPath = os.path.join(self.pkgPath, "win32")
-        self.executableResPath = os.path.join(self.winDist, "res")
+        self.macMetadataPath = os.path.join(self.pkgPath, "darwin")
+        self.executableResPath = os.path.join(self.macDist, "res")
 
     def _clearMainFolders(self):
         if os.path.exists(self.versionPath):
@@ -25,38 +25,34 @@ class WindowsPackager(Packager):
 
     def _makeMainDirs(self):
         os.makedirs(self.versionPath)
-        os.makedirs(self.winDist)
+        os.makedirs(self.macDist)
         os.makedirs(self.executableResPath)
         os.makedirs(self.installerPath)
 
     def _addResFilesForExecutable(self):
         copytree(os.path.join(self.resPath, "common"), self.executableResPath)
-        copytree(os.path.join(self.resPath, "windows"), self.executableResPath)
-
-    def _addBatScripsToWinDist(self):
-        batName = "afterInstall.bat"
-        shutil.copy2(self.versionPath + os.sep + batName, self.winDist + os.sep + batName)
+        copytree(os.path.join(self.resPath, "darwin"), self.executableResPath)
 
     def _addMetadataForInstaller(self):
-        copytree(self.winMetadataPath, self.versionPath)
-        self._addBatScripsToWinDist()
-
-    def _deleteVersionFolder(self):
-        if os.path.exists(self.versionPath):
-            shutil.rmtree(self.versionPath)
+        copytree(self.macMetadataPath, self.versionPath)
 
     def _constructAndMoveExecutable(self):
         currentPath = os.getcwd()
         os.chdir(self.srcPath)
         try:
             call(["pyinstaller", "--onefile", "web2board.py"])
-            shutil.copy2(os.path.join(self.pyInstallerDistFolder, "web2board.exe"), self.winDist)
-            shutil.copy2(os.path.join(self.pyInstallerDistFolder, "SerialMonitor.exe"), self.winDist)
+            call(["pyinstaller", "--onefile", "SerialMonitor.py"])
+            shutil.copy2(os.path.join(self.pyInstallerDistFolder, "web2board.exe"), self.macDist)
+            shutil.copy2(os.path.join(self.pyInstallerDistFolder, "SerialMonitor.exe"), self.macDist)
         finally:
             os.chdir(currentPath)
 
+    def _deleteVersionFolder(self):
+        if os.path.exists(self.versionPath):
+            shutil.rmtree(self.versionPath)
+
     def _moveInstallerToInstallerFolder(self):
-        shutil.copy2(self.versionPath + os.sep + "Web2board.exe", self.installerPath + os.sep + "Web2board.exe")
+        shutil.copy2(self.versionPath + os.sep + "web2board.exe", self.installerPath + os.sep + "Web2board.exe")
 
     def createPackage(self):
         try:
@@ -72,9 +68,9 @@ class WindowsPackager(Packager):
             self._addMetadataForInstaller()
             os.chdir(self.versionPath)
             log.info("Creating Installer")
-            call(["makensis", "installer.nsi"])
-            self._moveInstallerToInstallerFolder()
-            log.info("installer created successfully")
+            #call(["makensis", "installer.nsi"])
+            #self._moveInstallerToInstallerFolder()
+            #log.info("installer created successfully")
         finally:
             log.debug("Cleaning files")
             os.chdir(self.web2boardPath)
--- src/package.py
+++ src/package.py
@@ -1,7 +1,9 @@
 from libs.Packagers.LinuxPackager import LinuxPackager
 from libs.Packagers.WindowsPackager import WindowsPackager
+from libs.Packagers.MacPackager import MacPackager
 
 # packager = WindowsPackager()
-packager = LinuxPackager()
+# packager = LinuxPackager()
+packager = MacPackager()
 packager.createPackage()
 
