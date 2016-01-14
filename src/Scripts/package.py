87325b5444135491c6698ac753d21529e7199aae
Jorge Garcia Irazabal <jorge.garcia@bq-ms.local>
2016-01-14 10:20:51 +0100

    added metadata to scripts to be opened by default for python interpreter

--- src/Scripts/package.py
+++ src/Scripts/package.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import sys
 print os.getcwd()
--- src/Test/integration/testSerialConnection.py
+++ src/Test/integration/testSerialConnection.py
@@ -1,13 +1,15 @@
 import unittest
 
+from serial.serialutil import SerialException
+
 from SerialMonitor import SerialConnection
 from libs import utils
 
-class TestSerialConnection(unittest.TestCase):
 
+class TestSerialConnection(unittest.TestCase):
     def test_construct_isAbleToConnectToSerialPort(self):
         port = utils.listSerialPorts()[0][0]
         try:
             self.serialConnection = SerialConnection(port)
-        except Exception as e:
+        except SerialException as e:
             self.fail("unable to connect to port: {0} due to: {1}".format(port, e))
