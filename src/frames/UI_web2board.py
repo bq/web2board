83732862cb93f119d897ed5ccde19fad945f2c9c
Jorge García Irazábal <Jorge Garca Irazbal>
2016-01-17 18:28:16 +0100

    fixing name problems 02

--- src/frames/UI_web2board.py
+++ src/frames/UI_web2board.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\SoftwareProjects\web2board\src\frames\Ui_web2board2.ui'
+# Form implementation generated from reading ui file 'C:\SoftwareProjects\web2board\src\frames\UI_web2board.ui'
 #
-# Created: Sun Jan 17 18:26:21 2016
+# Created: Sun Jan 17 18:27:48 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
