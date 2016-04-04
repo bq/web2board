c1e8b630da6aeeeeb04bb1582d432825a139832f
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-04-04 09:08:44 +0200

    removed traces

--- src/web2board.py
+++ src/web2board.py
@@ -13,22 +13,14 @@
 #                                                                       #
 # -----------------------------------------------------------------------#
 
-print(0)
 import importlib
-print(1)
 import pprint
-print(2)
 import signal
-print(3)
 import click
-print(4)
 from wshubsapi.HubsInspector import HubsInspector
 
-print(5)
 from Scripts.TestRunner import *
-print(6)
 from libs.LoggingUtils import initLogging
-print(7)
 from libs.PathsManager import PathsManager
 
 log = initLogging(__name__)  # initialized in main
@@ -44,17 +36,13 @@ def getEchoFunction(original):
     return clickEchoForExecutable
 
 
-print(8)
 click.echo = getEchoFunction(originalEcho)
-print(9)
 click.secho = getEchoFunction(originalSEcho)
 
 def clickConfirm(message):
     print message
     return True
 
-print(10)
-print(10)
 click.confirm = clickConfirm
 
 
@@ -70,19 +58,13 @@ if "-Q" in sys.argv:
     runSconsScript()
 
 if __name__ == "__main__":
-    print(11)
     qtApp = None
     try:
-        print(12)
         importlib.import_module("libs.WSCommunication.Hubs")
-        print(13)
         HubsInspector.inspectImplementedHubs()
-        print(14)
         from libs.MainApp import getMainApp, forceQuit
-        print(15)
         app = getMainApp()
 
-        print(16)
         def closeSigHandler(signal, frame):
             try:
                 log.warning("closing server")
@@ -94,11 +76,8 @@ if __name__ == "__main__":
             os._exit(1)
 
 
-        print(17)
         signal.signal(signal.SIGINT, closeSigHandler)
-        print(18)
         qtApp = app.startMain()
-        print(19)
         sys.exit(qtApp.exec_())
     except SystemExit:
         pass
@@ -107,11 +86,7 @@ if __name__ == "__main__":
             print(20)
             raise e
         else:
-            print(21)
             log.critical("critical exception", exc_info=1)
-    print(22)
     if qtApp is not None:
-        print(23)
         qtApp.quit()
-    print(24)
     os._exit(1)
