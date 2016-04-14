376714eef020331250e3e16b7886f1e5c701d3ac
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-04-14 17:23:08 +0200

    fixed ConstructRes with different working directory

--- src/Scripts/ConstructRes.py
+++ src/Scripts/ConstructRes.py
@@ -1,30 +1,33 @@
+import sys
+
 import inspect
 import os
 import platform
 from os.path import join
-
 import shutil
 
-from libs import utils
 
 
 platformToFolder = dict(Linux="linux", Windows="windows", Darwin="darwin")
 
-
-modulePath = os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename)
+modulePath = os.path.abspath(os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename))
 os.chdir(modulePath)
-resPath = os.path.normpath(join(modulePath, os.pardir, os.pardir, "res"))
-srcPath = os.path.normpath(join(modulePath, os.pardir))
+resPath = os.path.abspath(os.path.normpath(join(modulePath, os.pardir, os.pardir, "res")))
+srcPath = os.path.abspath(os.path.normpath(join(modulePath, os.pardir)))
 
 resPlatformPath = join(resPath, platformToFolder[platform.system()])
 resCommonPath = join(resPath, "common")
 
 srcResPath = join(srcPath, "res")
 
+sys.path += [srcPath]
+
 if os.path.exists(srcResPath):
     shutil.rmtree(srcResPath)
 
 os.makedirs(srcResPath)
+
+from libs import utils
 
 utils.copytree(resCommonPath, srcResPath, forceCopy=True)
 utils.copytree(resPlatformPath, srcResPath, forceCopy=True)
