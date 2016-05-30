aa5d29e5fc6815d8d490db66f13e6f8a3df31d58
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-05-30 14:25:27 +0200

    minor changes

--- src/libs/Packagers/Packager.py
+++ src/libs/Packagers/Packager.py
@@ -21,9 +21,9 @@ class Packager:
     ARCH_64, ARCH_32 = "amd64", "i386"
 
     def __init__(self):
-        modulePath = utils.get_module_path()
-        self.packager_res_path = join(modulePath, "res")
-        self.web2board_path = os.path.abspath(join(modulePath, pardir, pardir, pardir))
+        module_path = utils.get_module_path()
+        self.packager_res_path = join(module_path, "res")
+        self.web2board_path = os.path.abspath(join(module_path, pardir, pardir, pardir))
         self.src_path = join(self.web2board_path, "src")
         self.res_path = join(self.web2board_path, "res")
         self.res_common_path = join(self.res_path, "common")
