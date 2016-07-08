59d78a3633e3e6af6a259bc30a16e03419a57c38
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-07-08 09:33:58 +0200

    adding headers and finished all communication

--- src/libs/WSCommunication/RequestHandler.py
+++ src/libs/WSCommunication/RequestHandler.py
@@ -9,27 +9,36 @@ log.addHandler(logging.NullHandler())
 
 app = Bottle()
 
-@app.hook('after_request')
-def enable_cors():
-    print "after_request hook"
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, x-requested-with'
+
+        if request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 
-
-@app.post('/')
+@app.route('/', method=['OPTIONS', 'POST'])
+@enable_cors
 def handle_message():
     comm_environment = CommEnvironment.get_instance()
-    message = ""
+    message = request.body.buf
+    replay_holder = dict()
 
     def on_message(msg):
-        global message
-        message = msg
+        replay_holder["msg"] = msg
 
     connected_client_mock = ConnectedClient(comm_environment, on_message)
+    comm_environment.on_message(connected_client_mock, message)
 
-    return message
+    return replay_holder["msg"]
 
 
 def start(options):
