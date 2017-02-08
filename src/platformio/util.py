2b56345af9d847352f5119c7c81899c359847839
Tom Calvo <calvo.tom@gmail.com>
2017-02-08 11:26:23 +0100

    Revert "get_source_dir of platformio util changed"
    This reverts commit 0780fb64da1b9d2d2578451c07df00e92d894096.

--- src/platformio/util.py
+++ src/platformio/util.py
@@ -181,8 +181,10 @@ def get_lib_dir():
 
 
 def get_source_dir():
-    return PathsManager.RES_PLATFORMIO_PATH # [JORGE_GARCIA] modified optimization
-
+    if utils.is_mac():
+        return PathsManager.RES_PLATFORMIO_PATH
+    else:
+        return dirname(realpath(__file__))
 
 
 def get_project_dir():
